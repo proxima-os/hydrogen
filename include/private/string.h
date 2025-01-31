caa5fd909473bda93d21a8ed5f700c5b6879caba
monkuous <84319635+monkuous@users.noreply.github.com>
2025-01-31 13:29:48 +0100

    use __builtin_mem*

--- include/private/string.h
+++ include/private/string.h
@@ -1,8 +1,6 @@
 #pragma once
 
-#include <stddef.h>
-
-int memcmp(const void *s1, const void *s2, size_t n);
-void *memcpy(void *restrict dest, const void *restrict src, size_t n);
-void *memmove(void *dest, const void *src, size_t n);
-void *memset(void *dest, int value, size_t n);
+#define memcmp __builtin_memcmp
+#define memcpy __builtin_memcpy
+#define memmove __builtin_memmove
+#define memset __builtin_memset
