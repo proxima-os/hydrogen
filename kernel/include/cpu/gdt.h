c18c22842f95982b3bd55861922161608061e126
monkuous <84319635+monkuous@users.noreply.github.com>
2025-02-24 23:03:36 +0100

    gdt: set cpl and rpl for user selectors to 3

--- kernel/include/cpu/gdt.h
+++ kernel/include/cpu/gdt.h
@@ -17,8 +17,8 @@ typedef struct {
 
 #define SEL_KCODE (offsetof(gdt_t, kern_code))
 #define SEL_KDATA (offsetof(gdt_t, kern_data))
-#define SEL_UDATA (offsetof(gdt_t, user_data))
-#define SEL_UCODE (offsetof(gdt_t, user_code))
+#define SEL_UDATA (offsetof(gdt_t, user_data) | 3)
+#define SEL_UCODE (offsetof(gdt_t, user_code) | 3)
 #define SEL_TSS (offsetof(gdt_t, tss_low))
 
 void init_gdt(struct cpu *cpu);
