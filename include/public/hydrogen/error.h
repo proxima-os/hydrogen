f09fe32a4cb6f9bd2b7ecb0d3cb3b2c214229fc6
monkuous <84319635+monkuous@users.noreply.github.com>
2025-02-27 11:57:11 +0100

    include/public: add doxygen documentation

--- include/public/hydrogen/error.h
+++ include/public/hydrogen/error.h
@@ -1,3 +1,7 @@
+/** \file
+ * Error codes.
+ */
+
 #ifndef HYDROGEN_ERROR_H
 #define HYDROGEN_ERROR_H
 
@@ -6,14 +10,16 @@ extern "C" {
 #endif
 
 typedef enum {
-    HYDROGEN_SUCCESS,
-    HYDROGEN_OUT_OF_MEMORY,
-    HYDROGEN_INVALID_ARGUMENT,
-    HYDROGEN_INVALID_FORMAT,
-    HYDROGEN_TIMED_OUT,
-    HYDROGEN_BUSY,
-    HYDROGEN_PAGE_FAULT,
-    HYDROGEN_ALREADY_EXISTS,
+    HYDROGEN_SUCCESS,          /**< Success. */
+    HYDROGEN_OUT_OF_MEMORY,    /**< Out of memory. */
+    HYDROGEN_INVALID_ARGUMENT, /**< Invalid argument. */
+    HYDROGEN_INVALID_FORMAT,   /**< Invalid input formatting. */
+    HYDROGEN_TIMED_OUT,        /**< Timed out. */
+    HYDROGEN_BUSY,             /**< Resource is unavailable. */
+    HYDROGEN_PAGE_FAULT,       /**< Page fault. */
+    HYDROGEN_ALREADY_EXISTS,   /**< Already exists. */
+    HYDROGEN_INVALID_HANDLE,   /**< Invalid handle. */
+    HYDROGEN_NO_PERMISSION,    /**< The operation is not allowed on the specified handle. */
 } hydrogen_error_t;
 
 #ifdef __cplusplus
--- include/public/hydrogen/memory.h
+++ include/public/hydrogen/memory.h
@@ -1,3 +1,7 @@
+/** \file
+ * Definitions for memory management.
+ */
+
 #ifndef HYDROGEN_MEMORY_H
 #define HYDROGEN_MEMORY_H
 
@@ -5,21 +9,21 @@
 extern "C" {
 #endif
 
-/*
- * Omitting a protection flag does not guarantee that the corresponding access type is not allowed. Specifically,
- * if any protection flag is given, `HYDROGEN_MEM_READ` and/or `HYDROGEN_MEM_EXEC` may be implied.
+/**
+ * Flags used when mapping memory.
+ *
+ * Note: if any protection flag is given, #HYDROGEN_MEM_READ and/or #HYDROGEN_MEM_EXEC may be implied.
  */
-
 typedef enum {
-    HYDROGEN_MEM_READ = 1 << 0,          /* Allow reading from this memory region. */
-    HYDROGEN_MEM_WRITE = 1 << 1,         /* Allow writing to this memory region. */
-    HYDROGEN_MEM_EXEC = 1 << 2,          /* Allow executing code in this memory region. */
-    HYDROGEN_MEM_SHARED = 1 << 3,        /* Propagate writes to the backing object. Illegal for anonymous mappings. */
-    HYDROGEN_MEM_NO_CACHE = 1 << 4,      /* Don't cache memory accesses. */
-    HYDROGEN_MEM_WRITE_COMBINE = 2 << 4, /* Use write-combining caching or stronger for this memory region. */
-    HYDROGEN_MEM_WRITE_THROUGH = 3 << 4, /* Use write-through caching or stronger for this memory region. */
-    HYDROGEN_MEM_EXACT = 1 << 6,         /* Fail if the mapping cannot be placed at the specified address. */
-    HYDROGEN_MEM_OVERWRITE = 1 << 7,     /* If combined with HYDROGEN_MEM_EXACT, remove existing mappings. */
+    HYDROGEN_MEM_READ = 1 << 0,          /**< Allow reading from this memory region. */
+    HYDROGEN_MEM_WRITE = 1 << 1,         /**< Allow writing to this memory region. */
+    HYDROGEN_MEM_EXEC = 1 << 2,          /**< Allow executing code in this memory region. */
+    HYDROGEN_MEM_SHARED = 1 << 3,        /**< Propagate writes to the backing object. Illegal for anonymous mappings. */
+    HYDROGEN_MEM_NO_CACHE = 1 << 4,      /**< Don't cache memory accesses. */
+    HYDROGEN_MEM_WRITE_COMBINE = 2 << 4, /**< Use write-combining caching or stronger for this memory region. */
+    HYDROGEN_MEM_WRITE_THROUGH = 3 << 4, /**< Use write-through caching or stronger for this memory region. */
+    HYDROGEN_MEM_EXACT = 1 << 6,         /**< Fail if the mapping cannot be placed at the specified address. */
+    HYDROGEN_MEM_OVERWRITE = 1 << 7,     /**< If combined with #HYDROGEN_MEM_EXACT, remove existing mappings. */
 } hydrogen_mem_flags_t;
 
 #ifdef __cplusplus
