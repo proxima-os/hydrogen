e32acd057b0e38207d2362196e7b145b4039b793
monkuous <84319635+monkuous@users.noreply.github.com>
2025-05-23 12:59:46 +0200

    proc: implement semaphores

---
+++ kernel/include/proc/semaphore.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "proc/mutex.h"
+#include "util/list.h"
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+typedef struct {
+    mutex_t lock;
+    size_t count;
+    list_t waiting;
+} semaphore_t;
+
+bool sema_try_wait(semaphore_t *sema);
+int sema_wait(semaphore_t *sema, bool interruptible, uint64_t deadline);
+void sema_signal(semaphore_t *sema);
+void sema_reset(semaphore_t *sema);
---
+++ kernel/proc/semaphore.c
@@ -0,0 +1,64 @@
+#include "proc/semaphore.h"
+#include "cpu/cpudata.h"
+#include "kernel/compiler.h"
+#include "proc/mutex.h"
+#include "proc/sched.h"
+#include "util/list.h"
+
+bool sema_try_wait(semaphore_t *sema) {
+    mutex_acq(&sema->lock, 0, false);
+
+    bool ok = sema->count > 0;
+    if (ok) sema->count -= 1;
+
+    mutex_rel(&sema->lock);
+    return ok;
+}
+
+int sema_wait(semaphore_t *sema, bool interruptible, uint64_t deadline) {
+    mutex_acq(&sema->lock, 0, false);
+
+    while (sema->count == 0) {
+        list_insert_tail(&sema->waiting, &current_thread->wait_node);
+        sched_prepare_wait(interruptible);
+        mutex_rel(&sema->lock);
+        int error = sched_perform_wait(deadline);
+        mutex_acq(&sema->lock, 0, false);
+        
+        if (unlikely(error)) {
+            list_remove(&sema->waiting, &current_thread->wait_node);
+            mutex_rel(&sema->lock);
+            return error;
+        }
+    }
+
+    sema->count -= 1;
+    mutex_rel(&sema->lock);
+    return 0;
+}
+
+void sema_signal(semaphore_t *sema) {
+    mutex_acq(&sema->lock, 0, false);
+
+    if (sema->count++ == 1) {
+        thread_t *thread = LIST_HEAD(sema->waiting, thread_t, wait_node);
+
+        while (thread) {
+            thread_t *next = LIST_NEXT(*thread, thread_t, wait_node);
+
+            if (sched_wake(thread)) {
+                list_remove(&sema->waiting, &thread->wait_node);
+            }
+
+            thread = next;
+        }
+    }
+
+    mutex_rel(&sema->lock);
+}
+
+void sema_reset(semaphore_t *sema) {
+    mutex_acq(&sema->lock, 0, false);
+    sema->count = 0;
+    mutex_rel(&sema->lock);
+}
